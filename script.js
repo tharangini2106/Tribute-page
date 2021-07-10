04c00feed47c6b10516d3db5454e49eaf4c31703
Tharangini <83527895+tharangini2106@users.noreply.github.com>
2021-07-10 00:24:22 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".dropdown-menu").click(function() {
+ 
+       $(".navbar-collapse").collapse("hide");
+   
+});
+
+$(".homeLink").click(function() {
+ 
+       $(".navbar-collapse").collapse("hide");
+   
+});
